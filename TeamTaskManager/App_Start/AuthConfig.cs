9c68fb8194688c738027d8f7ad14879aa5ab4a71
Rick Wicker <rickjr82@gmail.com>
2013-09-19 12:14:26 -0400

    Initial commit of empty project

---
+++ TeamTaskManager/App_Start/AuthConfig.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Web.WebPages.OAuth;
+using TeamTaskManager.Models;
+
+namespace TeamTaskManager
+{
+    public static class AuthConfig
+    {
+        public static void RegisterAuth()
+        {
+            // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
+            // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
+
+            //OAuthWebSecurity.RegisterMicrosoftClient(
+            //    clientId: "",
+            //    clientSecret: "");
+
+            //OAuthWebSecurity.RegisterTwitterClient(
+            //    consumerKey: "",
+            //    consumerSecret: "");
+
+            OAuthWebSecurity.RegisterFacebookClient(
+                 appId: "407277456043312",
+                 appSecret: "1728989b17195b86eb6a62b3404a49fa");
+
+            //OAuthWebSecurity.RegisterGoogleClient();
+        }
+    }
+}
