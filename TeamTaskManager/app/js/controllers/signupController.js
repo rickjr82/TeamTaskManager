5d989b15e14f22d33445431cbcdaf5974c4506ad
rickjr82 <rickjr82@gmail.com>
2013-10-02 22:00:26 -0400

    Correct checks and enabled on signup

--- TeamTaskManager/app/js/controllers/signupController.js
+++ TeamTaskManager/app/js/controllers/signupController.js
@@ -4,40 +4,63 @@
         $scope.players = [];
         $scope.tasks = [];
         $scope.games = [];
-        $scope.taskAssignments = {games:[] };
+        $scope.taskAssignmentArray = [];
+        $scope.taskAssignments = [];
         $scope.currentPlayerId = $rootScope.currentPlayerId;
+        $scope.currentPlayerId = 7;
         function refreshView() {
             $scope.$apply();
         }
-        $scope.findTaskAssignmentRecord= function(gameId, taskId) {
-            var game = _.findWhere($scope.taskAssignments.games, { gameId: gameId });
-            var gameTask = _.findWhere(game.tasks, { taskId: taskId });
-            return taskAssignment;
+        $scope.findTaskAssignment = function (gameId, taskId) {
+           return  _.findWhere($scope.taskAssignmentArray, { gameId: gameId, taskId: taskId });
         };
-        $scope.teamId = $routeParams.teamId;
-        dataservice.getEntities('Players', $scope.players, refreshView, [{ typeQ: 'where', first: 'teamId', second: 'eq', third: $scope.teamId }]);
-        dataservice.getEntities('Tasks', $scope.tasks, refreshView, [{ typeQ: 'where', first: 'teamId', second: 'eq', third: $scope.teamId }],true,setTaskAssignments);
-        dataservice.getEntities('Games', $scope.games, refreshView, [{ typeQ: 'where', first: 'teamId', second: 'eq', third: $scope.teamId }], true, setTaskAssignments);
-        dataservice.getEntities('TaskAssignments', $scope.taskAssignments, refreshView, [{ typeQ: 'where', first: 'Game.TeamId', second: 'eq', third: $scope.teamId }, { typeQ: 'expand', first:'Game' }], true, setTaskAssignments);
         $scope.setTaskAssignments = function () {
-            if ($scope.games.length > 0 && $scope.tasks.length>0) {
-                $scope.taskAssignments.games = [];
+            if ($scope.games.length > 0 && $scope.tasks.length > 0) {
                 _.each($scope.games, function (game) {
-                    var newGame={ gameId: game.id, tasks:[] };
-                    _.each($scope.tasks, function(task){
-                        newGame.tasks.append({taskId: task.id});
+                    _.each($scope.tasks, function (task) {
+                        $scope.taskAssignmentArray.push({ gameId:game.id, playerId: null, taskId:task.id });
                     });
-                    $scope.taskAssignments.games.push();
                 });
-                if ($scope.taskAssignments.length > 0)
-                {
-                    _.each($scope.taskAssignments, function (taskAssignment) {
-                        var localTask = $scope.findTaskAssignmentRecord(taskAssignment.gameId, taskAssignment.taskId);
-                        localTask.playerId = taskAssignment.playerId;
+                if ($scope.taskAssignments.length > 0) {
+                    _.each($scope.taskAssignments, function (taskAssignmentFromDb) {
+                        var taskAssignment = $scope.findTaskAssignment(taskAssignmentFromDb.gameId, taskAssignmentFromDb.taskId);
+                        taskAssignment.playerId = taskAssignmentFromDb.playerId;
                     });
+                    refreshView();
                 }
             }
         };
+        $scope.teamId = $routeParams.teamId;
+        $scope.taskIsAssigned = function (gameId, taskId) {
+            if ($scope.taskAssignmentArray.length == 0) {
+                return false;
+            }
+            var taskAssignment=$scope.findTaskAssignment(gameId, taskId);
+            return taskAssignment.playerId !== null;
+        };
+        $scope.taskIsNotAbleToBeAssigned = function (gameId, taskId) {
+            if ($scope.taskAssignmentArray.length == 0) {
+                return true;
+            }
+            var taskAssignment = $scope.findTaskAssignment(gameId, taskId);
+            return taskAssignment.playerId !== $scope.currentPlayerId && taskAssignment.playerId !==null;
+        };
+
+        dataservice.getEntities('Players', $scope.players, refreshView, [{ typeQ: 'where', first: 'teamId', second: 'eq', third: $scope.teamId }]);
+        dataservice.getEntities('Tasks', $scope.tasks, refreshView, [{ typeQ: 'where', first: 'teamId', second: 'eq', third: $scope.teamId }],true,$scope.setTaskAssignments);
+        dataservice.getEntities('Games', $scope.games, refreshView, [{ typeQ: 'where', first: 'teamId', second: 'eq', third: $scope.teamId }], true, $scope.setTaskAssignments);
+        dataservice.getEntities('TaskAssignments', $scope.taskAssignments, refreshView, [{ typeQ: 'where', first: 'game.teamId', second: 'eq', third: $scope.teamId }, { typeQ: 'expand', first: 'Game' }], true, $scope.setTaskAssignments);
+        $scope.setTaskAssignment = function (gameId, taskId) {
+            var localTask = $scope.findTaskAssignment(gameId, taskId);
+            if (localTask.playerId == $scope.currentPlayerId) {
+                localTask.playerId = null;
+            }
+            else {
+                localTask.playerId = $scope.currentPlayerId
+            }
+            
+        };
+        
         $scope.close = function () {
             $location.path('/admin');
         };
